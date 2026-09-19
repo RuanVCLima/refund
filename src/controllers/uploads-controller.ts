85f3087c13944e0c0519d5d0edf28a8bea6c0e62
Ruan Victor <ruanvclima@gmail.com>
2026-09-19 16:39:24 -0300

    feat: created the disk-storage to upload the files on the upload folder

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express"
-import { z } from "zod"
+import { z, ZodError } from "zod"
 import uploadConfig from "@/configs/upload"
+import { DiskStorage } from "@/providers/disk-storage"
+import { AppError } from "@/utils/AppError"
 
 class UploadsController{
     async create(request: Request, response: Response){
+        const diskStorage = new DiskStorage()
 
         try{
             const fileSchema = z.looseObject({
@@ -18,11 +21,21 @@ class UploadsController{
                     .refine((size) => size <=uploadConfig.MAX_FILE_SIZE, `File exceeds the maximun size of ${uploadConfig.MAX_SIZE}`)
             })
 
-            const {file} = fileSchema.parse(request.file)
+            const file = fileSchema.parse(request.file)
+
+            const filename = await diskStorage.saveFile(file.filename)
 
 
-            response.json({message: "ok"})
+            response.json({filename})
         } catch(error){
+
+            if(error instanceof ZodError){
+                if(request.file){
+                    await diskStorage.deleteFile(request.file.filename, "tmp")
+                }
+
+                throw new AppError(error.issues[0].message)
+            }
             throw error
         }
     }

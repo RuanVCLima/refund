bff346feff461f4ec2da653ffb44c4aeee2cd937
Ruan Victor <ruanvclima@gmail.com>
2026-09-19 08:18:39 -0300

    feat: validated user data to register on to the database

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -1,7 +1,27 @@
 import {Request, Response} from "express"
+import { z } from "zod"
 
 class RefundController{
     async create(request: Request, response: Response){
+
+        const CategoriesEnum = z.enum([
+            "food",
+            "services", 
+            "transport", 
+            "accomodation", 
+            "others"
+        ])
+        
+        const bodySchema = z.object({
+            name: z.string().trim().min(1, {message: "Name is required"}),
+            category: CategoriesEnum,
+            amount: z.number().positive({message: "The value needs to be positive"}),
+            filename: z.string().min(20)
+        })
+
+        const { name, category, amount, filename } = bodySchema.parse(request.body)
+
+
         response.json({message: "ok"})
     }
 }

bd49cb4259a5c5462d81a5bcacc7eb6c07fe15ba
Ruan Victor <ruanvclima@gmail.com>
2026-09-19 07:30:07 -0300

    feat: verified the role

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { RefundController } from "@/controllers/refund-controller";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const refundRoutes = Router()
 const refundController = new RefundController()
 
-refundRoutes.post("/", refundController.create)
+refundRoutes.post(
+    "/",
+    verifyUserAuthorization(["employee"]),
+    refundController.create)
 
 export { refundRoutes}

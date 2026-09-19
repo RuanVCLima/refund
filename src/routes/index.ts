be7440eb99c006fc974b24c9d645e68217ff1577
Ruan Victor <ruanvclima@gmail.com>
2026-09-19 07:17:53 -0300

    feat: verified the sessions token in order to enter the refund database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundRoutes } from "./refunds-routes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router()
 
@@ -10,6 +11,8 @@ const routes = Router()
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
+//private routes
+routes.use(ensureAuthenticated)
 routes.use("/refunds", refundRoutes)
 
 
